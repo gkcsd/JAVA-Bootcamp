e0be41f58cad49404775135de90b914e63678672
Ganesh Kale <kaleg2025@gmail.com>
2021-01-17 18:15:33 +0530

    Inheritance Section added,Upcasting & DownCasting Added

--- src/oops/inheritance/MainClass.java
+++ src/oops/inheritance/MainClass.java
@@ -2,25 +2,27 @@ package oops.inheritance;
 
 public class MainClass {
     public static void main(String[] args) {
-        Teacher t = new Teacher();
-        t.name = "Mr. Garry";
-
-        t.teach();
-
-        t.walk();
-
-        t.eat();
+//        Teacher t = new Teacher();
+//        t.name = "Mr. Garry";
+//
+//        t.teach();
+//
+//        t.walk();
+//
+//        t.eat();
+//
+//        System.out.println();
+//
+//        Singer s = new Singer();
+//        s.name = "Harry";
+//
+//        s.sing();
+//
+//        s.eat();
+//
+//        s.walk();
 
         System.out.println();
-
-        Singer s = new Singer();
-        s.name = "Harry";
-
-        s.sing();
-
-        s.eat();
-
-        s.walk();
 
         //upcasting:
         //          teacher is a person
@@ -32,5 +34,22 @@ public class MainClass {
         //          Person may be teacher but it will show the runtime error
 //        Person p = new Person();
 //        Teacher t = (Teacher) p;
+
+        //Simplified Casting:
+        Teacher t1 = new Teacher();
+
+        Singer s1 = new Singer();
+
+        Person p = t1;
+
+        Teacher t = (Teacher) p;
+
+        boolean ans = t1 instanceof Teacher;
+
+        System.out.println(t1 instanceof Teacher);
+        System.out.println(s1 instanceof Singer);
+        System.out.println(t1 instanceof Person);
+
+        System.out.println(t instanceof Teacher);
     }
 }
