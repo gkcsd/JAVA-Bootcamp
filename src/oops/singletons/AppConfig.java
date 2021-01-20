c4d9523cab3580b98bd4132e3844fb42bea3721b
Ganesh Kale <kaleg2025@gmail.com>
2021-01-20 22:25:57 +0530

    Singleton Pattern Section Added

--- src/oops/singletons/AppConfig.java
+++ src/oops/singletons/AppConfig.java
@@ -1,4 +1,18 @@
 package oops.singletons;
 
 public class AppConfig {
+
+    private AppConfig() {
+
+    }
+
+    private static AppConfig obj = null;
+
+    public static AppConfig getInstance() {
+
+        if (obj == null) {
+            obj = new AppConfig();      // it is lazy but optimized method..
+        }
+        return obj;
+    }
 }
--- src/oops/singletons/MainClass.java
+++ src/oops/singletons/MainClass.java
@@ -3,5 +3,9 @@ package oops.singletons;
 public class MainClass {
     public static void main(String[] args) {
 
+        AppConfig obj = new AppConfig.getInstance();
+        AppConfig obj2 = new AppConfig.getInstance();
+        AppConfig obj3 = new AppConfig.getInstance();
+
     }
 }
