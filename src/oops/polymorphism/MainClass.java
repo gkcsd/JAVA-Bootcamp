a52ab38f9c2ea2914f61db47fd50c43ff52c7d4c
Ganesh Kale <kaleg2025@gmail.com>
2021-01-24 17:18:07 +0530

    Polymorphism Added-runtime Poly

---
+++ src/oops/polymorphism/MainClass.java
@@ -0,0 +1,13 @@
+package oops.polymorphism;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+
+        Pet p = d;
+        Animal a = d;
+
+        d.walk();
+        p.walk(); // it is runtime polymorphism..
+    }
+}
--- src/twopointersalgorithm/TwoPointersAlgorithm.java
+++ src/twopointersalgorithm/TwoPointersAlgorithm.java
@@ -1,45 +1,45 @@
-package twopointersalgorithm;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class TwoPointersAlgorithm {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-->0) {
-            int n = sc.nextInt();
-            int[] a = new int[n];
-            for(int i = 0; i<n;i++) {
-                a[i]=sc.nextInt();
-            }
-            Triplet g = new Triplet();
-            if(g.findTriplets(a,n)) {
-                System.out.println("1");
-            } else {
-                System.out.println("0");
-            }
-        }
-    }
-}
-
-class Triplet{
-    public boolean findTriplets(int arr[], int n) {
-        arr.sort();
-    }
-
-    public boolean twoSum(int a[], int x) {
-        int i = 0, j = a.length - 1;
-
-        while (i < j) {
-            if (a[i] + a[j] > x) {
-                j--;
-            } else if(a[i] + a[j] < x) {
-                i++;
-            } else {
-                return true;
-            }
-        }
-        return false;
-    }
-}
+//package twopointersalgorithm;
+//
+//import java.util.Arrays;
+//import java.util.Scanner;
+//
+//public class TwoPointersAlgorithm {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int t = sc.nextInt();
+//        while(t-->0) {
+//            int n = sc.nextInt();
+//            int[] a = new int[n];
+//            for(int i = 0; i<n;i++) {
+//                a[i]=sc.nextInt();
+//            }
+//            Triplet g = new Triplet();
+//            if(g.findTriplets(a,n)) {
+//                System.out.println("1");
+//            } else {
+//                System.out.println("0");
+//            }
+//        }
+//    }
+//}
+//
+//class Triplet{
+//    public boolean findTriplets(int arr[], int n) {
+//        arr.sort();
+//    }
+//
+//    public boolean twoSum(int a[], int x) {
+//        int i = 0, j = a.length - 1;
+//
+//        while (i < j) {
+//            if (a[i] + a[j] > x) {
+//                j--;
+//            } else if(a[i] + a[j] < x) {
+//                i++;
+//            } else {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+//}
