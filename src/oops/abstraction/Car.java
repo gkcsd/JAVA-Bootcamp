a606c48ebf9aeefd7f655112b0e17fd6158263b6
Ganesh Kale <kaleg2025@gmail.com>
2021-01-25 20:50:21 +0530

    Abstraction Section Added

---
+++ src/oops/abstraction/Car.java
@@ -0,0 +1,12 @@
+package oops.abstraction;
+
+public abstract class Car {
+    public abstract void accelerate();
+
+    public abstract void breaking();
+
+    //Concrete method..
+    public void Honk() {
+        System.out.println("car is Honking");
+    }
+}
