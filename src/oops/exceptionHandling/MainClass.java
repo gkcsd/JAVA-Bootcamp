ef4c6cec8a3ae6d5d68d08e7d450ac66997eba72
Ganesh Kale <kaleg2025@gmail.com>
2021-01-29 18:00:07 +0530

    Exception Handler Section Added

--- src/oops/exceptionHandling/MainClass.java
+++ src/oops/exceptionHandling/MainClass.java
@@ -3,19 +3,24 @@ package oops.exceptionHandling;
 public class MainClass {
     public static void main(String[] args) {
 
-//        try {
-//            int a = 5;
-//            int b = 0;
-//
-//            int c = a/b;
-//            System.out.println(c);
-//        } catch (ArithmeticException e) {
-//            System.out.println(e.getMessage() + " , please check the code");
-//        }
-//
-//        System.out.println("This is the outer code");
-//        System.out.println("This need to run");
-        fun1();
+        try {
+            int a = 5;
+            int b = 0;
+
+            int c = a/b;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage() + " , please check the code");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Index should be in range of 0 to size of array");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e + " please check the code");
+        }
+
+        System.out.println("This is the outer code");
+        System.out.println("This need to run");
+
+//        fun1();
     }
         static void fun1() {
             int a = 5;
