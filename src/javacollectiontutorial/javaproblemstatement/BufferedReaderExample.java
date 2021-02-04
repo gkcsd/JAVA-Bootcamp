38d802d24640a54f7259fa6fd9923df5ba18e4ab
Ganesh Kale <kaleg2025@gmail.com>
2021-02-04 16:38:03 +0530

    BufferedReaderExample Section Added

---
+++ src/javacollectiontutorial/javaproblemstatement/BufferedReaderExample.java
@@ -0,0 +1,19 @@
+package javacollectiontutorial.javaproblemstatement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderExample {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter a String");
+
+        String s = br.readLine();
+
+        System.out.println("You Entered " + s);
+
+    }
+}
