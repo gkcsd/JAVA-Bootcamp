631a5cd576f7887854335e3fddaebcaba47fd2c1
Ganesh Kale <kaleg2025@gmail.com>
2021-02-06 22:00:50 +0530

    FunctionThree Example Added

---
+++ src/javacollectiontutorial/javaproblemstatement/FunctionThree.java
@@ -0,0 +1,33 @@
+package javacollectiontutorial.javaproblemstatement;
+
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+class Poi
+{
+    int x;
+    int y;
+}
+public class FunctionThree
+{
+    public static void main(String[] args)throws IOException
+    {
+
+        Poi p = new Poi();
+
+        p.x = 5; p.y = 10;
+
+        fun(p);
+
+        System.out.println(p.x +" "+ p.y);
+    }
+
+    public static void fun(Poi p)
+    {
+        p.x = 10; p.y = 10;
+    }
+
+
+}
+
