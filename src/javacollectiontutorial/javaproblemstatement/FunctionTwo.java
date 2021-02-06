948c063bdc82d8b10573cb3ec2f1940bc0c2fd0c
Ganesh Kale <kaleg2025@gmail.com>
2021-02-06 21:56:58 +0530

    FunctionTwo Example Added

---
+++ src/javacollectiontutorial/javaproblemstatement/FunctionTwo.java
@@ -0,0 +1,29 @@
+package javacollectiontutorial.javaproblemstatement;
+
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+
+public class FunctionTwo
+{
+    public static void main(String[] args)throws IOException
+    {
+        int x = 5, y = 10;
+
+        System.out.println(getMax(x, y));
+    }
+
+    public static int getMax(int x, int y)
+    {
+        if(x > y)
+            return x;
+
+        else
+            return y;
+    }
+
+
+}
+
+
