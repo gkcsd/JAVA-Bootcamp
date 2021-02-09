df2de0ea0312e4661eec7e4633f1c1f2f16c8990
Ganesh Kale <kaleg2025@gmail.com>
2021-02-09 20:51:11 +0530

    PrimitiveTypeArray Example Added

---
+++ src/javacollectiontutorial/javaproblemstatement/PrimitiveTypeArray.java
@@ -0,0 +1,18 @@
+package javacollectiontutorial.javaproblemstatement;
+
+import java.util.Scanner;
+
+public class PrimitiveTypeArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float[] b = new float[4];
+
+        for (int i = 0; i < b.length; i++) {
+            b[i] = sc.nextFloat();
+        }
+        for (int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
+
+    }
+}
