b514642ed6caf65e70416db49fb7e5888c128dcc
Ganesh Kale <kaleg2025@gmail.com>
2021-02-11 20:55:34 +0530

    FibonacciSeries Program Added

---
+++ src/PracticePoblems/FibonacciSeries.java
@@ -0,0 +1,17 @@
+package PracticePoblems;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+
+        int n = 100, t1 = 0, t2 = 1;
+        System.out.print("Up to " + n + ": ");
+
+        while (t1<= n)
+        {
+            System.out.print(t1 + " + ");
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
+}
