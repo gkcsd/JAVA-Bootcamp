aed9660abd14ad6b72bdddc819d9baed7e9ca367
Ganesh Kale <kaleg2025@gmail.com>
2021-02-14 14:08:44 +0530

    ArrayUserInput Example Added

---
+++ src/javacollectiontutorial/javaproblemstatement/ArrayUserInput.java
@@ -0,0 +1,32 @@
+package javacollectiontutorial.javaproblemstatement;
+
+import java.util.Scanner;
+
+public class ArrayUserInput {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int itemNum;
+
+        System.out.println("Enter The Length Of The Array: ");
+        itemNum = in.nextInt();
+
+        int items[] = new int[itemNum];
+
+        for (int i = 0; i < items.length; ++i) {
+            System.out.println("Enter The Array Elements: ");
+            items[i] = in.nextInt();
+        }
+
+        System.out.print("The Values are: [");
+        for (int i = 0; i < items.length; ++i) {
+            if (i == 0) {
+                System.out.print(items[0]);
+            } else {
+                System.out.print(", " + items[i]);
+            }
+        }
+        System.out.print("]");
+        in.close();
+    }
+}
