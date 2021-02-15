aa2ba8bd96b3de0cfc77ac0afa17d38575870f30
Ganesh Kale <kaleg2025@gmail.com>
2021-02-15 16:12:27 +0530

    BubbleSort Example Added + Optimized It

--- src/javacollectiontutorial/javaproblemstatement/BubbleSort.java
+++ src/javacollectiontutorial/javaproblemstatement/BubbleSort.java
@@ -3,17 +3,24 @@ package javacollectiontutorial.javaproblemstatement;
 class BubbleSortClass {
     public void sortArray(int arr[]) {
         int n = arr.length;
+        boolean swapped;
 
         for (int i = 0; i < n; i++) {
+            swapped = false;
             for (int j = 0; j < n-i-1; j++) {
                 if (arr[j] > arr[j+1]) {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;
                 }
+            }
+            if (swapped == false) {
+                break;
             }
         }
     }
+
     public void printArray(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n; i++) {
