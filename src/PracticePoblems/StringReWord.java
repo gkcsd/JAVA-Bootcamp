9b438e7f001ce5ab377a8b753458ae37fa01c871
Ganesh Kale <kaleg2025@gmail.com>
2021-02-23 22:12:48 +0530

    StringReWord

---
+++ src/PracticePoblems/StringReWord.java
@@ -0,0 +1,17 @@
+package PracticePoblems;
+
+
+public class StringReWord {
+    public static void main(String[] args) {
+// TODO Auto-generated method stub
+        String str = "Welcome To Tansai";
+        String[] strArray = str.split(" ");
+        for (String temp: strArray){
+            System.out.println(temp);
+        }
+        for(int i=0; i<3; i++){ char[] s1 = strArray[i].toCharArray(); for (int j = s1.length-1; j>=0; j--)
+        {System.out.print(s1[j]);}
+            System.out.print(" ");
+        }
+    }
+}
