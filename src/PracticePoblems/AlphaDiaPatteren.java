bd896a239b3fd9ef7a15742ed204804dd1d67a10
Ganesh Kale <kaleg2025@gmail.com>
2021-02-23 22:01:46 +0530

    AlphaDiaPatteren

---
+++ src/PracticePoblems/AlphaDiaPatteren.java
@@ -0,0 +1,32 @@
+package PracticePoblems;
+
+import java.util.Scanner;
+
+public class AlphaDiaPatteren {
+    // Java program to print alphabet A pattern
+    void display(int n)
+    {
+// Outer for loop for number of lines
+        for (int i = 0; i<=n; i++) {
+// Inner for loop for logic execution
+            for (int j = 0; j<= n / 2; j++) {
+// prints two column lines
+                if ((j == 0 || j == n / 2) && i != 0 ||
+// print first line of alphabet
+                        i == 0  && j != n / 2 ||
+// prints middle line
+                        i == n / 2)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        AlphaDiaPatteren a = new AlphaDiaPatteren();
+        a.display(7);
+    }
+}
