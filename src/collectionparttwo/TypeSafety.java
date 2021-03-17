6b7dca61961975002b81f1976745f7d3c338c9f0
gkcsd <kaleg2025@gmail.com>
2021-03-17 13:11:26 +0530

    TypeSafety With & Without Generic Added

--- src/collectionparttwo/TypeSafety.java
+++ src/collectionparttwo/TypeSafety.java
@@ -1,16 +1,32 @@
 package collectionparttwo;
-//Without generics it shows the runtime error and generates ClassCastException
-class Pair {
-    object x;
-    object y;
+////Without generics it shows the runtime error and generates ClassCastException
+//class Pair {
+//    object x;
+//    object y;
+//}
+//
+//public class TypeSafety {
+//    public static void main(String[] arg) {
+//        Pair p = new Pair();
+//        p.x = 10;
+//        p.y = "Ganesh";
+//        String s = (String)p.x;
+//    }
+//}
+
+//with generics it shows compiler time error which more understandable than runtime error
+
+class PairTwo<T,S> {
+    T x;
+    S y;
 }
 
 public class TypeSafety {
-    public static void main(String[] arg) {
-        Pair p = new Pair();
+    public static void main(String[] args) {
+        PairTwo<Integer,String> p = new PairTwo<Integer,String>();
         p.x = 10;
-        p.y = "Ganesh";
-        String s = (String)p.x;
+        p.y = "Ram";
+        String str = (String)p.y;
     }
 }
 
