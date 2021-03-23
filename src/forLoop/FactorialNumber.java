bd81bc4c0a9441e973e58a445d61fa478e798a14
gkcsd <kaleg2025@gmail.com>
2021-03-23 13:20:18 +0530

    Problem statement added

--- src/forLoop/FactorialNumber.java
+++ src/forLoop/FactorialNumber.java
@@ -5,16 +5,14 @@ import java.util.Scanner;
 public class FactorialNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter the factorial number: ");
+        System.out.println("Enter the number: ");
         int n = sc.nextInt();
 
         int fact = 1;
+
         for (int i = n; i >= 1; i--) {
             fact *= i;
-            System.out.println("The factorial count is " + fact);
         }
-        System.out.println("The sum of factorial number is: " + fact);
-
+        System.out.println("The factorial of " + n + " is " + fact);
     }
 }
