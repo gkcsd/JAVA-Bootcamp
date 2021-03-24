752d479c818fbbf34f1f32e27c25e4a0cedfed44
gkcsd <kaleg2025@gmail.com>
2021-03-24 13:56:42 +0530

    PrintcollectionWildChar program added

--- src/collectionparttwo/wildcard/PrintCollecionWildChar.java
+++ src/collectionparttwo/wildcard/PrintCollecionWildChar.java
@@ -1,15 +1,16 @@
-package collectionparttwo.wildcard;
-
-import java.util.Collection;
-
-public class PrintCollecionWildChar {
-    public static void main(String[] args) {
-        printCollection(c);
-    }
-
-    public static void printCollection(Collection<?> c) {
-        for (object e: c) {
-            System.out.println(c + " ");
-        }
-    }
-}
+//package collectionparttwo.wildcard;
+//
+//import java.util.ArrayList;
+//import java.util.Collection;
+//
+//public class PrintCollecionWildChar {
+//    public static void main(String[] args) {
+////        printCollection(c);
+//    }
+//
+//    public static void printCollection(ArrayList<?> c) {
+//        for (object e: c) {
+//            System.out.println(c + " ");
+//        }
+//    }
+//}
